b73a68a37dd0c1db06935447b384653dc5c734d9
Kurt Kreilinger <k.kreilinger@gmail.com>
2020-04-25 15:12:34 +0200

    Merge origin/master
    Conflicts:
    	src/class1/NewClass.java

---
+++ src/class1/Class3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author Kurt
+ */
+public class Class3 {
+    
+}
